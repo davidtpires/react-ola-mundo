1b07d7d1ab21a9fb19f70b15d80a7ffc859830bc
David T. <davidt.info@gmail.com>
2025-03-10 13:51:20 -0300

    Links do react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
+
       <Routes>
-        <Route path="/" element={<Inicio />}/>
-        <Route path="/sobremim" element={<SobreMim />}/>
-        <Route path="*" element={<div>Página não encontrada</div>}/>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
     </BrowserRouter>
   );
