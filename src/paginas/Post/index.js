1b422b2f8c5ebc56a472098682411a0595b83e86
David T. <davidt.info@gmail.com>
2025-03-12 08:51:56 -0300

    Tratando Post não encontrado

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,31 +1,44 @@
 import './Post.css'
-import { useParams } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 import posts from "json/posts.json"
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from 'react-markdown'
+import NaoEncontrada from 'paginas/NaoEncontrada';
+import PaginaPadrao from 'componentes/PaginaPadrao';
 
 export default function Post() {
-    const parametros = useParams();
+  const parametros = useParams();
 
-    const post = posts.find((post) => {
-        return post.id === Number(parametros.id);
-    })
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  })
 
-    if (!post) {
-      return <h1>Post não encontrado...</h1>
-    }
+  if (!post) {
+    return <NaoEncontrada />
+  }
 
-    return (
-      <PostModelo
-        fotoCapa={`/assets/posts/${post.id}/capa.png`}
-        titulo={post.titulo}
-      >
-        <div className="post-markdown-container">
-          <ReactMarkdown>
-            {post.texto}
-          </ReactMarkdown>
-        </div>
-        
-      </PostModelo>
-    )
+  return (
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route index element={
+          <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+          >
+            <div className="post-markdown-container">
+              <ReactMarkdown>
+                {post.texto}
+              </ReactMarkdown>
+            </div>
+
+          </PostModelo>
+
+
+        } />
+
+      </Route>
+
+    </Routes>
+
+  )
 }
